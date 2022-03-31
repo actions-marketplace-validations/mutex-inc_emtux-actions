83cf7a73cb8a8bd33591704caa190e1d8ed145c7
dfc22 <48900989+dfc22@users.noreply.github.com>
2022-04-01 08:31:45 +0900

    一旦

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as yaml from 'js-yaml';
 import { ExecOptions } from '@actions/exec';
 
 const main = async () => {
   try {
     // inputのJSONオブジェクトを取得する
     // core.getInputの戻り値はstringのため、JSON.parseが必要
-    const jsonObject = JSON.parse(core.getInput('json-data'));
-    let message = '';
+    const jsonObject = JSON.parse(core.getInput('params'));
 
-    // 文字列の生成（aaとccc/fffの文字列をつなげて「これは表示テスト」を作る）
-    message = jsonObject.aaa + jsonObject.ccc.fff; // outputの設定
-    core.setOutput('result-message', message);
+    const repo = jsonObject.types_repo;
+
+    const api = yaml.load(core.getInput('api'));
+
+    core.setOutput('result-message', `repo: ${repo}\napi: ${api}`);
   } catch (error) {
     core.setFailed(error as Error);
   }
